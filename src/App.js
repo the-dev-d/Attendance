425b08d8902c919d014ecdf5c0419542d70ced0c
Dhaivath Lal <dhaivathl@gmail.com>
2021-07-25 12:30:38 +0530

    seperate components made content.js index.html

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
-import logo from './logo.svg';
 import './App.css';
+import {useState} from 'react'
 
 function App() {
+  const [state, setstate] = useState(false)
+  console.log("react building..");
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {state?<h3>The Dev D</h3>:<h3>Dhaivath</h3>}
+      <button onClick={()=> {setstate(!state)}}>Hello</button>
+      <section>
+
+      </section>
     </div>
   );
 }
